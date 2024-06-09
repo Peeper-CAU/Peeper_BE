b2be47a7d7939c360640062e73aecd4f50efa41a
abt576 <limut0913@gmail.com>
2024-06-10 02:05:26 +0900

    Update VoceService : Fixed sendWavDataToAIServer method

--- src/main/java/com/cau/peeper/service/VoiceService.java
+++ src/main/java/com/cau/peeper/service/VoiceService.java
@@ -1,48 +1,72 @@
 package com.cau.peeper.service;
 
+import com.cau.peeper.dto.AnalysisRequest;
+import com.cau.peeper.dto.AnalysisResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
-import java.net.Socket;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Service
 public class VoiceService {
 
-    private static final String AI_SERVER_HOST = "ai.server.host";
-    private static final int AI_SERVER_PORT = 12345;
+    private static final String AI_SERVER_URL = "https://peeper-ai.dev-lr.com/wavAnalysis";
+    private final ObjectMapper objectMapper;
+
+    public VoiceService(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     public void processAudioFile(String uid, byte[] wavData) {
-        boolean result = sendWavDataToAIServer(wavData);
+        AnalysisRequest analysisRequest = new AnalysisRequest(uid, wavData);
+        AnalysisResponse analysisResponse = sendWavDataToAIServer(analysisRequest);
 
-        if (result) {
-            sendNotificationToFirebase(uid);
+        Boolean messageSending = analysisResponse.getMessageSending();
+        String riskLevel = analysisResponse.getRiskLevel();
+        if (messageSending) {
+            sendNotificationToFirebase(uid, riskLevel);
         }
     }
 
-    private boolean sendWavDataToAIServer(byte[] wavData) {
-        try (Socket socket = new Socket(AI_SERVER_HOST, AI_SERVER_PORT);
-             OutputStream outputStream = socket.getOutputStream();
-             InputStream inputStream = socket.getInputStream();
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+    private AnalysisResponse sendWavDataToAIServer(AnalysisRequest analysisRequest) {
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL(AI_SERVER_URL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
 
-            outputStream.write(wavData);
-            outputStream.flush();
+            String jsonRequest = objectMapper.writeValueAsString(analysisRequest);
 
-            String response = reader.readLine();
-            return Boolean.parseBoolean(response);
+            try (OutputStream outputStream = connection.getOutputStream()) {
+                outputStream.write(jsonRequest.getBytes());
+                outputStream.flush();
+            }
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String response = reader.readLine();
+                return objectMapper.readValue(response, AnalysisResponse.class);
+            }
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
+            return new AnalysisResponse(false, "Error");
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
     }
 
-    private void sendNotificationToFirebase(String uid) {
+    private void sendNotificationToFirebase(String uid, String riskLevel) {
         Message message = Message.builder()
-//                .putData("title", "Phishing Analysis Result")
-                .putData("body", "It is suspected to be phishing")
+                .putData("title", "Phishing Analysis Result")
+                .putData("body", riskLevel)
                 .setTopic(uid)
                 .build();
 
