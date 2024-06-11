ffc1dc46c29ca6102d9c61e83294272c7c5c852c
abt576 <limut0913@gmail.com>
2024-06-11 17:41:25 +0900

    Updated Project : Added Constructor

--- src/main/java/com/cau/peeper/config/WebSocketConfig.java
+++ src/main/java/com/cau/peeper/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.cau.peeper.config;
 
 import com.cau.peeper.handler.WebSocketHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -12,10 +13,17 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    private final WebSocketHandler webSocketHandler;
+
+    @Autowired
+    public WebSocketConfig(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         log.info("Registering WebSocket handler.");
-        registry.addHandler(new WebSocketHandler(), "/").setAllowedOrigins("*").withSockJS();
+        registry.addHandler(webSocketHandler, "/").setAllowedOrigins("*").withSockJS();
         log.info("WebSocket handler registered successfully.");
     }
 }
