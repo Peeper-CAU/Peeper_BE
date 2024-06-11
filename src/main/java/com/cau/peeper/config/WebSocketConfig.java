f0c37d86e890d1b8d37b74556ee1490f2f59fd6f
abt576 <limut0913@gmail.com>
2024-06-11 16:55:02 +0900

    Update WebSocketConfig : Added SockJS

--- src/main/java/com/cau/peeper/config/WebSocketConfig.java
+++ src/main/java/com/cau/peeper/config/WebSocketConfig.java
@@ -1,17 +1,21 @@
 package com.cau.peeper.config;
 
 import com.cau.peeper.handler.WebSocketHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+@Slf4j
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new WebSocketHandler(), "/").setAllowedOrigins("*");
+        log.info("Registering WebSocket handler.");
+        registry.addHandler(new WebSocketHandler(), "/").setAllowedOrigins("*").withSockJS();
+        log.info("WebSocket handler registered successfully.");
     }
 }
