b320c279e67e69a460f58198dcafc97c6142abc5
abt576 <limut0913@gmail.com>
2024-06-11 18:00:05 +0900

    Update WebSocketConfig : Added Constructor

--- src/main/java/com/cau/peeper/config/WebSocketConfig.java
+++ src/main/java/com/cau/peeper/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.cau.peeper.config;
 
 import com.cau.peeper.handler.WebSocketHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -15,7 +14,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     private final WebSocketHandler webSocketHandler;
 
-    @Autowired
     public WebSocketConfig(WebSocketHandler webSocketHandler) {
         this.webSocketHandler = webSocketHandler;
     }
